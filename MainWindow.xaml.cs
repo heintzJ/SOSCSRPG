ac3c9bf8cacc1d3b0b74754040bc96b59570d622
Jack Heintz <jheintz222@gmail.com>
2024-06-22 13:06:22 -0400

    reformatting player class properties

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.ViewModels;
 
 namespace WPFUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// Create a new GameSession object that will create a player object
+    /// The _gameSession is used to provide data for the .xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession;
         public MainWindow()
         {
             InitializeComponent();
+
+            _gameSession = new GameSession();
+
+            DataContext = _gameSession;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
         }
     }
 }
